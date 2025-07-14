fc51933ba4c33587e67ebaab7b7d4ffd02c2338f
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2025-07-14 17:57:29 -0500

    Asteroids split when hit or are destroyed when small enough

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,5 +12,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            rand_angle = random.uniform(20, 50)
+            new_pos_velocity = pygame.math.Vector2.rotate(self.velocity, rand_angle)
+            new_neg_velocity = pygame.math.Vector2.rotate(self.velocity, -rand_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            as1 = Asteroid(self.position.x, self.position.y, new_radius)
+            as2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+            as1.velocity = new_pos_velocity * 1.2
+            as2.velocity = new_neg_velocity * 1.2
 
     
--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
 
             for shot in shots:
                 if asteroid.is_colliding(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
 
