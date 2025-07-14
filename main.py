70f22f8344c226dc52449bf15de6753d47e57012
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2025-07-14 15:56:18 -0500

    Update 60 times per second

--- main.py
+++ main.py
@@ -2,24 +2,20 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    update(screen)
+    clock = pygame.time.Clock()
+    dt = 0
 
-def update(scr):
-    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(scr, "black")
+        pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
