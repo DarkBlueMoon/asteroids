f4058271b4929aa53ce36dd3e7a78c848882ca75
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2025-07-14 16:11:10 -0500

    Rotate player left and right

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen, "black")
+        player.update(dt)
+            
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
